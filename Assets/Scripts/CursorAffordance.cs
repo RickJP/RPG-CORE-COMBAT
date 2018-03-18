a45e65722a148b70ddbc490a54b173a8622bf919
RickJP <rickd1977@gmail.com>
2018-03-18 16:25:24 +0900

    Completed direct and click movement controls, added cursors

---
+++ Assets/Scripts/CursorAffordance.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D targetCursor = null;
+    [SerializeField] Texture2D unknownCursor = null;
+
+    [SerializeField] Vector2 cursorHotspot = new Vector2(96, 96);
+
+    CameraRaycaster cameraRayCaster;
+
+	// Use this for initialization
+	void Start () {
+        cameraRayCaster = GetComponent<CameraRaycaster>();		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        switch (cameraRayCaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Debug.LogError("Don't know what cursor it is.");
+                return;
+
+        }
+
+        
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,15 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
-{
-
-
+{  
     [SerializeField] float walkMoveStopRadius = 0.2f;
-
 
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
+    bool isInDirectMode = false;   // todo consider making static
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -21,10 +20,47 @@ public class PlayerMovement : MonoBehaviour
         currentClickTarget = transform.position;
     }
 
+
+
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKeyDown(KeyCode.G))    // todo     G for gamePad.  Allow player to map later or add to menu    
+        {
+            isInDirectMode = !isInDirectMode;
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+        
+    }
+
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        print(h + v);
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+
+    private void ProcessMouseMovement()
+    {
+        if (Input.GetMouseButton(0))            // pressed left mouse button
         {
             print("Cursor raycast hit" + cameraRaycaster.layerHit);
 
@@ -40,7 +76,7 @@ public class PlayerMovement : MonoBehaviour
                     print("Unexpected layer found");
                     return;
 
-            }        
+            }
         }
         var playerToClickPoint = currentClickTarget - transform.position;
 
